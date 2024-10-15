ee99f5bc098659d4b2d778688bf8a7b7467f96b4
Michal Grzadkowski <mgrzad@gmail.com>
2024-10-15 16:57:43 -0400

    updating opusDSD FSC per conformation script for refactored APIs

--- metrics/per_conf_fsc/cdrgn.py
+++ metrics/per_conf_fsc/cdrgn.py
@@ -68,7 +68,7 @@ def main(args: argparse.Namespace) -> None:
             subprocess.check_call(cmd, shell=True)
 
     if args.calc_fsc_vals:
-        utils.get_fsc_curves(outdir, args)
+        utils.get_fsc_curves(outdir, args.gt_dir, args.mask, args.fast, args.overwrite)
 
 
 if __name__ == "__main__":

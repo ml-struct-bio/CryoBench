0e3ac4133288ffd2d439772002af5935c86d38f5
Michal Grzadkowski <mgrzad@gmail.com>
2024-10-17 17:54:41 -0400

    merging Ribosembly FSC/conformation pipeline with that for the other datasets

--- metrics/per_conf_fsc/utils.py
+++ metrics/per_conf_fsc/utils.py
@@ -14,6 +14,27 @@ from cryodrgn import analysis, fft, mrc
 logger = logging.getLogger(__name__)
 
 
+# Ribosembly number of images per Ribosembly structure (total 16 structures)
+RIBOSEMBLY_NUM_IMGS = [
+    9076,
+    14378,
+    23547,
+    44366,
+    30647,
+    38500,
+    3915,
+    3980,
+    12740,
+    11975,
+    17988,
+    5001,
+    35367,
+    37448,
+    40540,
+    5772,
+]
+
+
 def get_fsc_cutoff(fsc_curve: pd.DataFrame, t: float) -> float:
     """Find the resolution at which the FSC curve first crosses a given threshold."""
     fsc_indx = np.where(fsc_curve.fsc < t)[0]
@@ -31,11 +52,21 @@ def numfile_sortkey(s):
     return parts
 
 
-def get_nearest_z_array(zmat: np.ndarray, num_vols: int, num_imgs: int) -> np.ndarray:
+def get_nearest_z_array(
+    zmat: np.ndarray, num_vols: int, num_imgs: Union[int, str]
+) -> np.ndarray:
     z_lst = []
     z_mean_lst = []
     for i in range(num_vols):
-        z_nth = zmat[(i * num_imgs) : ((i + 1) * num_imgs)]
+        if isinstance(num_imgs, int):
+            z_nth = zmat[(i * num_imgs) : ((i + 1) * num_imgs)]
+        elif num_imgs == "ribo":
+            z_nth = zmat[
+                sum(RIBOSEMBLY_NUM_IMGS[:i]) : sum(RIBOSEMBLY_NUM_IMGS[: (i + 1)])
+            ]
+        else:
+            raise ValueError(f"{num_imgs=}")
+
         z_nth_avg = z_nth.mean(axis=0)
         z_nth_avg = z_nth_avg.reshape(1, -1)
         z_lst.append(z_nth)
@@ -43,10 +74,14 @@ def get_nearest_z_array(zmat: np.ndarray, num_vols: int, num_imgs: int) -> np.nd
 
     nearest_z_lst = []
     centers_ind_lst = []
+    num_img_for_centers = 0
     for i in range(num_vols):
         nearest_z, centers_ind = analysis.get_nearest_point(z_lst[i], z_mean_lst[i])
         nearest_z_lst.append(nearest_z.reshape(nearest_z.shape[-1]))
-        centers_ind_lst.append(centers_ind)
+        centers_ind_lst.append(centers_ind + num_img_for_centers)
+
+        if num_imgs == "ribo":
+            num_img_for_centers += RIBOSEMBLY_NUM_IMGS[i]
 
     return np.array(nearest_z_lst)
 
@@ -66,8 +101,8 @@ def pad_mrc_vols(mrc_volfiles: Iterable[str], new_D: int) -> None:
         new[i : (i + x), j : (j + y), k : (k + z)] = v
 
         # adjust origin
-        apix = header.apix
-        xorg, yorg, zorg = header.origin
+        apix = header.get_apix()
+        xorg, yorg, zorg = header.get_origin()
         xorg -= apix * k
         yorg -= apix * j
         zorg -= apix * i

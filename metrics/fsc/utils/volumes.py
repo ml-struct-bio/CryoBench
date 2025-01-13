fix per-img fsc calculation

metrics/fsc/utils/volumes.py
@@ -15,7 +15,7 @@ from typing import Optional, Callable, Union
 import numpy as np
 import pandas as pd
 import torch
-from cryodrgn import fft, models, mrc
+from cryodrgn import fft, models, mrcfile
 
 logger = logging.getLogger(__name__)
 
@@ -178,7 +178,7 @@ def get_fsc_curve(
 
     maskvol = None
     if mask_file is not None:
-        maskvol = torch.tensor(mrc.parse_mrc(mask_file)[0])
+        maskvol = torch.tensor(mrcfile.parse_mrc(mask_file)[0])
 
     # Apply the given mask before applying the Fourier transform
     maskvol1 = vol1 * maskvol if maskvol is not None else vol1.clone()
@@ -265,8 +265,8 @@ def get_fsc_curves(
             continue
 
         out_fsc = os.path.join(outdir, outlbl, f"{ii:03d}.txt")
-        vol1 = torch.tensor(mrc.parse_mrc(gt_volfile)[0])
-        vol2 = torch.tensor(mrc.parse_mrc(vol_files[ii])[0])
+        vol1 = torch.tensor(mrcfile.parse_mrc(gt_volfile)[0])
+        vol2 = torch.tensor(mrcfile.parse_mrc(vol_files[ii])[0])
 
         if os.path.exists(out_fsc) and not overwrite:
             if ii % 20 == 0: